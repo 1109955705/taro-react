6e519c79b2297ea9a2b1db0cad84350aca856894
zhisonggang <zhisonggang@yolanda.hk>
2021-06-01 17:24:17 +0800

    update

---
+++ src/hooks/useIsMount.ts
@@ -0,0 +1,13 @@
+import { useRef, useEffect } from 'react';
+
+// 判断是否是首次加载, 比如useEffect首次不加载
+export default () => {
+
+  const isMountRef = useRef(false)
+
+  useEffect(() => {
+    isMountRef.current = true
+  }, [])
+
+  return isMountRef.current
+};
