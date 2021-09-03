d5e822a8ebd10b45ef5ea867b325462cc67aea99
1109955705 <q1109955705@163.com>
2021-09-03 19:20:18 +0800

    Keep calm and commit

--- src/pages/chart/index.tsx
+++ src/pages/chart/index.tsx
@@ -13,7 +13,7 @@ const Chart = () => {
   const {
     userinfo: { id },
     sessionKey,
-  } = useSelector((state: ReduxRootState) => state);
+  } = suseSelector((state: ReduxRootState) => state);
   const themeColor = useSelector((state: ReduxRootState) => state.themeColor);
 
   useEffect(() => {
